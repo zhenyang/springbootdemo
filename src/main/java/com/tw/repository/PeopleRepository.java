2ccfb8bb41ab4ea3ed76ff9fc9fc386d096c8556
zyang <zyang@thoughtworks.com>
2016-05-24 16:48:20 +0800

    customise logback

--- src/main/java/com/tw/repository/PeopleRepository.java
+++ src/main/java/com/tw/repository/PeopleRepository.java
@@ -2,6 +2,8 @@ package com.tw.repository;
 
 import com.tw.domain.People;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeopleRepository extends MongoRepository<People, String> {
 }
--- src/main/java/com/tw/repository/TodoRepository.java
+++ src/main/java/com/tw/repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.tw.repository;
 
 import com.tw.domain.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
 }
